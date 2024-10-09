65b317948f9361530174553ef598677dd72a6a0e
TLC-RQ <116693927+TLC1301-1020@users.noreply.github.com>
2024-10-09 00:11:53 -0400

    R2 Test
    Game distributes 12 adventure cards to each player, update the deck

--- A1-Cao-Tina-101158711/src/test/java/org/example/GameTest.java
+++ A1-Cao-Tina-101158711/src/test/java/org/example/GameTest.java
@@ -8,7 +8,7 @@ class GameTest {
 
     @Test
     @DisplayName("R1 Test")
-    void testSetupAdventureAndEventDeck() {
+    void testSetupDecks() {
         Game game = new Game();
         game.setupPlayers();
         boolean isDeckSetUp = game.setupDecks();
@@ -17,5 +17,21 @@ class GameTest {
         Assertions.assertTrue(isDeckSetUp, "Decks not set up.");
         Assertions.assertTrue(isDeckIntegrityVerified, "Deck not verified.");
         Assertions.assertTrue(arePlayersReady, "Players not ready.");
+    }
+
+    @Test
+    @DisplayName("R2 Test: Distribute 12 adventure cards to each player and update the deck")
+    void AdventureCardsToPlayers() {
+        Game game = new Game();
+        game.setupPlayers();
+
+
+        boolean isCardsDistributed = game.distributeAdventureCards();
+        int playerCardCount = game.getPlayerCardCount();
+        int remainingDeckCount = game.getRemainingDeckCount();
+
+        Assertions.assertTrue(isCardsDistributed, "Cards not distributed.");
+        Assertions.assertEquals(12, playerCardCount, "Each player do not have 12 cards.");
+        Assertions.assertEquals(remainingDeckCount, expectedDeckCount, "Deck not updated correctly.");
     }
 }
