c737d7f75182865db211176ab02650994b189492
TLC-RQ <116693927+TLC1301-1020@users.noreply.github.com>
2024-10-09 16:42:45 -0400

    Re - R1 Test

--- A1-Cao-Tina-101158711/src/test/java/org/example/GameTest.java
+++ A1-Cao-Tina-101158711/src/test/java/org/example/GameTest.java
@@ -1,37 +1,32 @@
 package org.example;
 
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class GameTest {
 
-    @Test
-    @DisplayName("R1 Test")
-    void testSetupDecks() {
-        Game game = new Game();
-        game.setupPlayers();
-        boolean isDeckSetUp = game.setupDecks();
-        boolean isDeckIntegrityVerified = game.verifyDeckIntegrity();
-        boolean arePlayersReady = game.checkPlayerReadiness();
-        Assertions.assertTrue(isDeckSetUp, "Decks not set up.");
-        Assertions.assertTrue(isDeckIntegrityVerified, "Deck not verified.");
-        Assertions.assertTrue(arePlayersReady, "Players not ready.");
+class GameTest {
+    private Game game;
+
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+        game.startGame();
     }
 
     @Test
-    @DisplayName("R2 Test: Distribute 12 adventure cards to each player and update the deck")
-    void AdventureCardsToPlayers() {
-        Game game = new Game();
-        game.setupPlayers();
+    @DisplayName("R1 Test")
+    //Game set up adventure and event deck, verifies the players and deck are initialized correctly
+    void testDecksInitialized() {
+        Assertions.assertNotNull(game.getDeck().getAdventureDeck(), "Adventure not initialized");
+        Assertions.assertNotNull(game.getDeck().getEventDeck(), "Event deck not initialized");
+
+        Assertions.assertEquals(100, game.getDeck().getAdventureDeckSize(), "Initial adventure deck not 100 cards.");
+        Assertions.assertEquals(4, game.getPlayers().size(), "Game does not have exactly 4 players");
 
 
-        boolean isCardsDistributed = game.distributeAdventureCards();
-        int playerCardCount = game.getPlayerCardCount();
-        int remainingDeckCount = game.getRemainingDeckCount();
-
-        Assertions.assertTrue(isCardsDistributed, "Cards not distributed.");
-        Assertions.assertEquals(12, playerCardCount, "Each player do not have 12 cards.");
-        Assertions.assertEquals(remainingDeckCount, expectedDeckCount, "Deck not updated correctly.");
     }
 }
