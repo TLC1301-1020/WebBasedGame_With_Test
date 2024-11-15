e91e133bab9b548620926f6215394788c8bbdb03
TLC-RQ <116693927+TLC1301-1020@users.noreply.github.com>
2024-11-14 23:59:28 -0500

    revise folder
    The previous files were messy

---
+++ A2-Cao-Tina-101158711/src/test/java/org/example/A1ScenarioSteps.java
@@ -0,0 +1,217 @@
+package org.example;
+
+import io.cucumber.java.en.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class A1ScenarioSteps {
+    private Menu menu;
+    private Game game;
+    private Deck mockDeck;
+    private Scanner mockScanner;
+
+    @Given("first game is created with players hands set to values")
+    public void first_game_is_created_with_players_hands_set_to_values() {
+        mockDeck = mock(Deck.class);
+        mockScanner = mock(Scanner.class);
+        game = new Game() {
+            @Override
+            public Deck getDeck() {
+                return mockDeck;
+            }
+        };
+
+
+        List<String> hand1 = Arrays.asList("F5", "F5", "F15", "F15", "D5", "S10", "S10", "H10", "H10", "B15", "B15", "L20");
+        List<String> hand2 = Arrays.asList("F5", "F5", "F5", "F10", "F10", "F10", "D5", "H10", "B15", "B15", "L20", "E30");
+        List<String> hand3 = Arrays.asList("F5", "F5", "F5", "F15", "D5", "S10", "H10", "S10", "H10", "B15", "L20", "S10");
+        List<String> hand4 = Arrays.asList("F5", "F15", "F15", "F40", "D5", "D5", "S10", "H10", "H10", "B15", "L20", "E30");
+        for (int i = 0; i < game.getPlayers().size(); i++) {
+            game.getPlayers().get(i).getHand().clear();
+        }
+
+        game.getPlayers().get(0).addCards(hand1);
+        game.getPlayers().get(1).addCards(hand2);
+        game.getPlayers().get(2).addCards(hand3);
+        game.getPlayers().get(3).addCards(hand4);
+
+
+
+        when(mockDeck.drawAdventureCard())
+                .thenReturn("F30","S10","B15") // p1s1
+                .thenReturn("F10","L20","L20") // p1s2
+                .thenReturn("B15","S10") // p3s3
+                .thenReturn("F30","L20") // p3s4
+                .thenReturn("S10"); //p2 draws
+
+
+        menu = new Menu(game);
+        mockScanner = mock(Scanner.class);
+        menu.setScanner(mockScanner);
+        menu.updateRound();
+
+    }
+
+    @When("player 1 declines to sponsor the quest {string}, player2 sponsors")
+    public void first_player_draws_a_quest_player2_sponsors(String quest){
+        when(mockScanner.nextInt()).thenReturn(2)
+                                    .thenReturn(1);
+        menu.findingSponsor(quest);
+
+    }
+
+    @When("three players participate the quest")
+    public void three_players_participate_the_quest(){
+        when(mockScanner.nextInt()).thenReturn(1)
+                                    .thenReturn(1)
+                                    .thenReturn(1);
+        menu.findParticipants();
+    }
+
+    @When("player2 sets the stage for {string}")
+    public void sponsor_sets_the_stage(String quest){
+        Player sponsor = menu.getSponsorplayer();
+        List<Player> participants = menu.getParticipants();
+        when(mockScanner.nextLine())
+                        .thenReturn("F10")
+                        .thenReturn("quit")
+
+                        .thenReturn("F10")
+                        .thenReturn("B15")
+                        .thenReturn("quit")
+
+                        .thenReturn("F10")
+                        .thenReturn("B15")
+                        .thenReturn("H10")
+                        .thenReturn("quit")
+
+                        .thenReturn("F5")
+                        .thenReturn("L20")
+                        .thenReturn("E30")
+                        .thenReturn("quit");
+        menu.buildQuest(quest,sponsor,participants);
+    }
+
+    @When("quest {string} completed with sponsor card updated and participant shields updated")
+    public void quest_completed_sponsor_card_updated_and_shields_updated(String event) {
+
+        when(mockScanner.nextLine())
+                .thenReturn("F5")   //dis p1
+                .thenReturn("no")
+                .thenReturn("D5")
+                .thenReturn("S10")
+                .thenReturn("quit")
+                .thenReturn("\n")
+
+                .thenReturn("F5")   //dis
+                .thenReturn("no")
+                .thenReturn("S10")
+                .thenReturn("D5")
+                .thenReturn("quit")
+                .thenReturn("\n")
+
+                .thenReturn("F5")   //dis
+                .thenReturn("no")
+                .thenReturn("D5")
+                .thenReturn("H10")
+                .thenReturn("quit")
+                .thenReturn("\n")
+
+                //p1 stage 2
+                .thenReturn("no")
+                .thenReturn("H10")
+                .thenReturn("S10")
+                .thenReturn("quit")
+                .thenReturn("\n")
+
+                //p3 stage 2
+                .thenReturn("no")
+                .thenReturn("B15")
+                .thenReturn("S10")
+                .thenReturn("quit")
+                .thenReturn("\n")
+
+                //p4 stage 2
+                .thenReturn("no")
+                .thenReturn("H10")
+                .thenReturn("B15")
+                .thenReturn("quit")
+                .thenReturn("\n")
+
+                //p3 stage 3
+                .thenReturn("no")
+                .thenReturn("L20")
+                .thenReturn("H10")
+                .thenReturn("S10")
+                .thenReturn("quit")
+                .thenReturn("\n")
+
+                //p4 stage 3
+                .thenReturn("no")
+                .thenReturn("D5")
+                .thenReturn("S10")
+                .thenReturn("L20")
+                .thenReturn("quit")
+                .thenReturn("\n")
+
+                //p3 stage 4
+                .thenReturn("no")
+                .thenReturn("B15")
+                .thenReturn("H10")
+                .thenReturn("L20")
+                .thenReturn("quit")
+                .thenReturn("\n")
+                //p4 stage 4
+                .thenReturn("no")
+                .thenReturn("D5")
+                .thenReturn("S10")
+                .thenReturn("L20")
+                .thenReturn("E30")
+                .thenReturn("quit")
+                .thenReturn("\n")
+
+                //ended player 2 trim
+                .thenReturn("S10")
+                .thenReturn("S10")
+                .thenReturn("S10")
+                .thenReturn("S10")
+                .thenReturn("S10")
+                .thenReturn("\n");
+
+        menu.quest(event);
+    }
+
+    @Then("sponsor player should have exact 12 cards")
+    public void sponsor_player_should_have_exact_cards(){
+        assertTrue(menu.getSponsorplayer().getHand().containsAll(List.of("F5","F5","D5","S10","S10","S10","S10","S10","S10","S10","S10","S10")));
+        assertEquals(menu.getSponsorplayer().getHand().size(),12);
+    }
+
+
+    @Then("shields should be updated correctly")
+    public void shields_should_be_updated_correctly(){
+        assertEquals(menu.getParticipants().size(),1);                       //only one participant left at the last stage
+        assertTrue(menu.getParticipants().contains(game.getPlayers().get(3)));     //only player 4 is in the participant list
+        assertEquals(game.getPlayers().get(0).getShields(),0);              //player 1 shield 0
+        assertEquals(game.getPlayers().get(1).getShields(),0);              //player 2 shield 0
+        assertEquals(game.getPlayers().get(2).getShields(),0);              //player 3 shield 0
+        assertEquals(game.getPlayers().get(3).getShields(),4);              //player 4 shield 4 as won the quest
+
+    }
+
+    @Then("cards in each player hand should be correct")
+    public void cards_in_each_player_hand_should_be_correct(){
+        assertTrue(game.getPlayers().get(0).getHand().containsAll(Arrays.asList("F5", "F10", "F15", "F15", "F30", "H10", "B15", "B15", "L20")));
+        assertTrue(game.getPlayers().get(2).getHand().containsAll(Arrays.asList("F5", "F5", "F15", "F30", "S10")));
+        assertTrue(game.getPlayers().get(3).getHand().containsAll(Arrays.asList("F15", "F15", "F40", "L20")));
+    }
+
+
+}
+
+
+
---
+++ A2-Cao-Tina-101158711/src/test/java/org/example/MenuTest.java
@@ -0,0 +1,124 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuTest {
+
+    @Test
+    @DisplayName("R3 - Check player order")
+    public void testUpdateRound(){
+        Game game = new Game();
+        Menu menu = new Menu(game);
+
+        menu.updateRound();
+        Assertions.assertEquals("Player1", menu.getCurrentplayer().getName());
+        menu.updateRound();
+        Assertions.assertEquals("Player2", menu.getCurrentplayer().getName());
+        menu.updateRound();
+        Assertions.assertEquals("Player3", menu.getCurrentplayer().getName());
+        menu.updateRound();
+        Assertions.assertEquals("Player4", menu.getCurrentplayer().getName());
+        //iterate back to player1
+        menu.updateRound();
+        Assertions.assertEquals("Player1", menu.getCurrentplayer().getName());
+    }
+
+    @Test
+    @DisplayName("R5.1 - Game affected by Plague")
+    public void testPlagueDrawn(){
+        Game game = new Game();
+        Menu menu = new Menu(game);
+        Player player = game.getPlayers().getFirst();
+
+        player.updateShields(3);
+        menu.setCurrentPlayer(player);
+        menu.plagueCard();
+        Assertions.assertEquals(1,player.getShields(),"Player should have one shield left");
+    }
+
+    @Test
+    @DisplayName("R5.2 - Game affected by Queen's Favor cards")
+    public void testQueenFavorDrawn(){
+        Game game = new Game();
+        Menu menu = new Menu(game);
+        Player player = game.getPlayers().getFirst();
+
+        player.getHand().clear();
+        menu.setCurrentPlayer(player);
+        menu.QueensFavor();
+        Assertions.assertEquals(2,menu.getCurrentplayer().getHand().size(),"There should be 2 cards in hand");
+    }
+
+    @Test
+    @DisplayName("R5.3 - Game affected by Prosperity")
+    public void testProsperity(){
+        Game game = new Game();
+        Menu menu = new Menu(game);
+        menu.setCurrentPlayer(game.getPlayers().getFirst());
+        menu.Prosperity();
+
+        Assertions.assertEquals(14,game.getPlayers().getFirst().getHand().size());
+        Assertions.assertEquals(14,game.getPlayers().get(1).getHand().size());
+        Assertions.assertEquals(14,game.getPlayers().get(2).getHand().size());
+        Assertions.assertEquals(14,game.getPlayers().get(3).getHand().size());
+    }
+
+    @Test
+    @DisplayName("R8 - Test detect trim needed")
+    public void testDetectTrimming(){
+        Game game = new Game();
+        Menu menu = new Menu(game);
+        Player p = game.getPlayers().getFirst();
+        System.out.println("Current player hand " + p.getHand());
+        p.getHand().add("H3");
+        menu.setCurrentPlayer(p);
+        System.out.println(menu.trimNeeded());
+    }
+
+    @Test
+    @DisplayName("R10.2 - Check if enough foe card by player to be sponsor")
+    public void enoughFoeCard(){
+        Game game = new Game();
+        Menu menu = new Menu(game);
+        Player player = game.getPlayers().getFirst();
+        player.getHand().clear();
+        player.addCards(List.of("F1","F3"));
+        menu.setCurrentPlayer(player);
+        Assertions.assertFalse(menu.enoughFoeCard(5));
+        Assertions.assertTrue(menu.enoughFoeCard(2));
+    }
+
+
+    @Test
+    @DisplayName("R11 - Test Stage initialization")
+    public void testStageInitialization(){
+        Game game = new Game();
+        Menu menu = new Menu(game);
+        Player play = game.getPlayers().getFirst();
+        menu.setCurrentPlayer(play);
+        play.getHand().clear();
+        play.addCards(List.of("F1","F3","H10","S20"));
+        Quest quest = new Quest("Q3",play,List.of(game.getPlayers().get(1),game.getPlayers().get(3)));
+
+        quest.initializeStages(0,"F1",List.of("H10","S20"));
+        Assertions.assertEquals("F1",quest.getStageAtLevel(1).getFoeCard());
+        Assertions.assertEquals(31,quest.getStageAtLevel(1).getTotalValue());
+    }
+
+    @Test
+    @DisplayName("R18 - discard and draw new cards for sponsor")
+    public void testSponsorDrawNewCards(){
+        String quest = "Q5";
+        Game game = new Game();
+        Menu menu = new Menu(game);
+        Player p = game.getPlayers().getFirst();
+        menu.sponsorDrawAdventureCard(p,Integer.parseInt(quest.substring(1)));
+    }
+
+}
